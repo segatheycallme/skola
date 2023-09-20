fad117bae0269a42a5e247535d12cd6bdf9ee5d2
segatcm <segatheycallme@proton.me>
2023-09-20 22:21:09 +0200

    prvi cas

---
+++ dijagonalakocke.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	double a;
+	scanf("%lf", &a);
+	printf("%lf", a * sqrt(3));
+	return 0;
+}
---
+++ sredina.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	float a,b;
+	scanf("%f %f", &a, &b);
+	printf("%f", (a + b)/2);
+	return 0;
+}
